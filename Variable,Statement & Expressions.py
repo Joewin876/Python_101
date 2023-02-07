46e6fd9a208df051335c8b5eaefcac168cd0a4ea
Joewin276 <115566698+Joewin276@users.noreply.github.com>
2023-02-08 00:29:29 +0800

    Update Variable,Statement & Expressions.py

--- Variable,Statement & Expressions.py
+++ Variable,Statement & Expressions.py
@@ -1,7 +1,5 @@
-#x = 100 #statement untuk membuat variable x
-#y = 20  #statement untuk membuat variable y
-#z = x + y * 2000 #ekspresi menjumlahkan x dengan y lalu di kali dengan angka 2000
-#print(z)
+x = 100 #statement untuk membuat variable x
+y = 20  #statement untuk membuat variable y
+z = x + y * 2000 #ekspresi menjumlahkan x dengan y lalu di kali dengan angka 2000
+print(z)
 
-x = 4 + 2
-print(x)
